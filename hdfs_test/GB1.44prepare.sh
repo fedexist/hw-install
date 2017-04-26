1e1450b98f43d10f61eb4d8a082c380cde1019b7
fleanend <edoardoferrante@hotmail.it>
2017-04-26 16:59:27 +0200

    testing #1

--- hdfs_test/GB1.44prepare.sh
+++ hdfs_test/GB1.44prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install zip
-wget http://data.gdeltproject.org/events/2003.zip /tmp/2003.zip
-unzip /tmp/2003.zip
-
+wget http://data.gdeltproject.org/events/2003.zip
+unzip 2003.zip
+rm -f 2003.zip
 python MC1.44.py
