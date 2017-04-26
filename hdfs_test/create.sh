c73e2c5ac7fc199e0af482d18ee5ab77ad7f2726
fleanend <edoardoferrante@hotmail.it>
2017-04-26 15:45:56 +0200

    create.sh fix #2

--- hdfs_test/create.sh
+++ hdfs_test/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python CreateFile.py
-hadoop fs -mkdir /user/
-hadoop fs -mkdir /user/admin/
-hadoop fs -mkdir /user/admin/testing/
-hadoop fs -put -f test /user/admin/testing/test
+HADOOP_USER_NAME=hdfs hadoop fs -mkdir /user/
+HADOOP_USER_NAME=hdfs hadoop fs -mkdir /user/admin/
+HADOOP_USER_NAME=hdfs hadoop fs -mkdir /user/admin/testing/
+HADOOP_USER_NAME=hdfs hadoop fs -put -f test /user/admin/testing/test
