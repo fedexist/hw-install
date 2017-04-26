2a9ae7b364f2bcd9e7f1df3ce8366d226d3bb844
fleanend <edoardoferrante@hotmail.it>
2017-04-26 17:29:33 +0200

    to quickfix once

--- hdfs_test/MC1.44.py
+++ hdfs_test/MC1.44.py
@@ -6,9 +6,9 @@ from timeit import default_timer as timer
 times = []
 
 for x in range(0,9):
-	start = timer
+	start = timer()
 	subprocess.Popen("HADOOP_USER_NAME=hdfs hadoop fs -put -f 2003.csv /user/admin/testing/2003.csv", shell=True)
-	end = timer
+	end = timer()
 	subprocess.Popen("HADOOP_USER_NAME=hdfs hadoop fs -rm /user/admin/testing/2003.csv", shell=True)
 	times.append(end-start)
 	
