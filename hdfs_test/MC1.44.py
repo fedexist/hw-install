040fbf5dfa13b89b2c3fadba94060d7b244f7dc0
fleanend <edoardoferrante@hotmail.it>
2017-04-27 16:15:42 +0200

    add hadoop fs -expunge to remove flush .Trash after test

--- hdfs_test/MC1.44.py
+++ hdfs_test/MC1.44.py
@@ -11,5 +11,6 @@ for x in range(0,9):
 	subprocess.Popen("HADOOP_USER_NAME=hdfs hadoop fs -rm -skipTrash /user/admin/testing/2003.csv", shell=True)
 	times.append(end-start)
 	
-
+subprocess.Popen("HADOOP_USER_NAME=hdfs hadoop fs -expunge", shell=True)
+	
 print "Average Speed for 1.44 GB in upload: " + str(sum(times)/len(times))
