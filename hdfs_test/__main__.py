100e4076726f8ffcc8a5684915caf44b063cd9d0
fleanend <edoardoferrante@hotmail.it>
2017-04-28 12:27:22 +0200

    fix #5

--- hdfs_test/__main__.py
+++ hdfs_test/__main__.py
@@ -7,8 +7,8 @@ from timeit import default_timer as timer
 # Parsing script arguments
 parser = argparse.ArgumentParser(description="Test throughput")
 parser.add_argument('-u', '--URL', help='URL of the dataset to use for testing, the file must be a single csv in a zip archive, if this parameter is not specified the dataset is assumed to have been downloaded already (default: blank)')
-parser.add_argument('-f', '--flush', help="With this parameter the script will only clean up the HDFS")
-parser.add_argument('-fa', '--flushAll', help="With this parameter the script will clean up the HDFS and local files")
+parser.add_argument('-f', '--flush', help="With this parameter the script will only clean up the HDFS", action="store_true")
+parser.add_argument('-fa', '--flushAll', help="With this parameter the script will clean up the HDFS and local files", action="store_true")
 parser.set_defaults(url='')
 args = parser.parse_args()
 
