6d67d6dbff6220e5c9069ec13722fe70990a6a77
fleanend <edoardoferrante@hotmail.it>
2017-05-02 19:00:36 +0200

    test#5

--- hdfs_test/test_with_pyspark.py
+++ hdfs_test/test_with_pyspark.py
@@ -10,8 +10,8 @@ APP_NAME = " HelloWorld of Big Data"
 ##OTHER FUNCTIONS/CLASSES
 
 def main(sc):
-	sc.setLogLevel("WARN")
-	f = open("/root/hw_pre_install/test.csv")
+   sc.setLogLevel("WARN")
+   f = open("/root/hw_pre_install/test.csv")
    #textFile = sc.textFile("file:///root/hw_pre_install/test.csv")
    textFile = sc.serialize(f)
    textFile.saveAsTextFile("hdfs:///user/admin/testing")
