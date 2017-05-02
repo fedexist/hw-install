f56abe181da1625f02dfe740d0ed9f94b0c554e0
fleanend <edoardoferrante@hotmail.it>
2017-05-02 19:30:37 +0200

    test#8

--- hdfs_test/test_with_pyspark.py
+++ hdfs_test/test_with_pyspark.py
@@ -5,6 +5,7 @@ from pyspark import SparkConf, SparkContext
 from operator import add
 import time
 import sys
+from timeit import default_timer as timer
 ## Constants
 APP_NAME = " HelloWorld of Big Data"
 ##OTHER FUNCTIONS/CLASSES
@@ -12,9 +13,12 @@ APP_NAME = " HelloWorld of Big Data"
 def main(sc):
    sc.setLogLevel("WARN")
    #f = open("/root/hw_pre_install/test.csv")
+   start = timer()
    textFile = sc.textFile("file:///root/hw-pre-install/test.csv")
    #textFile = sc.serialize(f)
    textFile.saveAsTextFile("hdfs:///user/admin/testing/tmp")
+   end = timer()
+   print end-start
 
 if __name__ == "__main__":
 
